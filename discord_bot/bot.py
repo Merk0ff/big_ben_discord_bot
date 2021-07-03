81f3ed56d18651b7b1f6a2ad65b7ac102a8ecc00
p.dukshtau <p.dukshtau@uptrader.io>
2021-07-03 22:39:02 +0300

    Add pretty messages and logging

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,3 +1,5 @@
+import logging
+
 import discord
 import pickledb
 
@@ -6,6 +8,10 @@ from discord.ext import commands
 from discord.errors import ClientException
 
 from datetime import datetime
+
+
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Music(commands.Cog):
@@ -17,16 +23,26 @@ class Music(commands.Cog):
         if not self.db.exists('channels'):
             self.db.lcreate('channels')
 
+        if not self.db.exists('guilds'):
+            self.db.lcreate('guilds')
+
         self.sched = AsyncIOScheduler()
         self.sched.add_job(self.big_boy, 'cron', minute='*/15')
-        self.sched.add_job(self.big_boy_leave, 'cron', minute='*/15', second='*/40')
+        self.sched.add_job(self.big_boy_leave, 'cron', minute='*/17')
         self.sched.start()
 
     @commands.command()
     async def subscribe(self, ctx):
-        channel = ctx.author.voice.channel
+        channel_id = ctx.author.voice.channel.id
+        guild_id = ctx.guild.id
 
-        self.db.ladd('channels', channel.id)
+        if guild_id not in self.db.lgetall('guilds'):
+            self.db.ladd('channels', channel_id)
+            self.db.ladd('guilds', guild_id)
+            logger.info(f'Subscribe guild: {ctx.guild.name}, {channel_id}')
+            await ctx.send('Subscribed')
+        else:
+            await ctx.send('You already subscribed, unsubscribe first')
 
     async def big_boy(self):
         time = datetime.now().strftime('%-I%M')
@@ -39,6 +55,7 @@ class Music(commands.Cog):
             except ClientException:
                 continue
 
+            logger.info(f'Played in {channel.name} at {time}')
             voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
 
             self.__active_voice_client.append(voice_client)
@@ -52,4 +69,10 @@ class Music(commands.Cog):
     @commands.command()
     async def unsubscribe(self, ctx):
         channel_id = ctx.author.voice.channel.id
+        guild_id = ctx.guild.id
+
         self.db.lremvalue('channels', channel_id)
+        self.db.lremvalue('guilds', guild_id)
+
+        await ctx.send('Unsubscribed')
+        logger.info(f'Unsubscribe guild: {ctx.guild.name}, {channel_id}')
