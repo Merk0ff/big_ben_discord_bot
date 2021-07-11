7fff4a636ca8da763cefd16e92c00a085b61964e
p.dukshtau <p.dukshtau@uptrader.io>
2021-07-11 17:41:22 +0300

    assword

--- db_integration/settings.py
+++ db_integration/settings.py
@@ -7,5 +7,6 @@ redis = StrictRedis(
     host=environ.get('REDIS_HOST'),
     port=int(environ.get('REDIS_PORT')),
     db=int(environ.get('REDIS_DB')),
+    password=environ.get('REDIS_PASSWORD', None),
     decode_responses=True,
 )
