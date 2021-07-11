4b6b4d8c9d23438d71cb0f8808b22c2e8a411b33
p.dukshtau <p.dukshtau@uptrader.io>
2021-07-11 17:44:35 +0300

    heroku

--- db_integration/settings.py
+++ db_integration/settings.py
@@ -2,11 +2,15 @@ from os import environ
 
 from redis import StrictRedis
 
+redis_url = environ.get('REDIS_URL', None)
 
-redis = StrictRedis(
-    host=environ.get('REDIS_HOST'),
-    port=int(environ.get('REDIS_PORT')),
-    db=int(environ.get('REDIS_DB')),
-    password=environ.get('REDIS_PASSWORD', None),
-    decode_responses=True,
-)
+if redis_url:
+    redis = StrictRedis.from_url(redis_url, decode_responses=True)
+else:
+    redis = StrictRedis(
+        host=environ.get('REDIS_HOST'),
+        port=int(environ.get('REDIS_PORT')),
+        db=int(environ.get('REDIS_DB')),
+        password=environ.get('REDIS_PASSWORD', None),
+        decode_responses=True,
+    )
